81b4d172846be14163ca5aa40c68fa7c1b60749b
ArneDewulf <arne.dewulf@student.howest.be>
2017-05-08 10:31:39 +0200

    Eerste Commit

---
+++ MovieShopXS_DewulfArne/src/MovieShopXS_DewulfArne/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieShopXS_DewulfArne.Controllers
+{
+    [Route("")]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        [Route("")]
+        [Route("[action]")]
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [Route("[action]/{year?}")]
+        public IActionResult Year(int year)
+        {
+            return View();
+        }
+
+        [Route("[action]/{id?}/Rating/{Up?}")]
+        public IActionResult Update(int rating, string Up)
+        {
+            return View();
+        }
+    }
+}
